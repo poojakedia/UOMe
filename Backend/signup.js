72bb71124cbc57eaef97ff7c3882c60c9d6b0775
poojakedia <poojakedianov@gmail.com>
2025-02-08 13:58:08 -0500

    sign up

--- Backend/signup.js
+++ Backend/signup.js
@@ -1,12 +1,13 @@
-import { setDoc, getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const auth = getAuth();
 const fdb = getFirestore(app);
 export const createUserHandler = async()=> {
     try{
         const response = await createUserWithEmailAndPassword(auth, email, password);
-                await setDoc(doc(fdb, 'users', auth.currentUser.uid), {
-                    name: '',
+                await setDoc(doc(fdb, 'users', fbauth.currentUser.uid), {
+                    firstName: '',
+                    lastName: '',
                     email: email,
                     password: password
                 });

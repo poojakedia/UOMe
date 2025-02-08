5f183527ba54239cc6bb898bab16f17901335752
Sanchita Agrawal <sanchitaagrawal362@gmail.com>
2025-02-08 15:09:29 -0500

    sign up update

--- Backend/signup.js
+++ Backend/signup.js
@@ -5,9 +5,8 @@ const fdb = getFirestore(app);
 export const createUserHandler = async()=> {
     try{
         const response = await createUserWithEmailAndPassword(auth, email, password);
-                await setDoc(doc(fdb, 'users', fbauth.currentUser.uid), {
-                    firstName: '',
-                    lastName: '',
+                await setDoc(doc(fdb, 'users', auth.currentUser.uid), {
+                    name: '',
                     email: email,
                     password: password
                 });

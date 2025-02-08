5e0ac10699a805d6b9ae5882638b3735626be02f
poojakedia <poojakedianov@gmail.com>
2025-02-08 13:58:08 -0500

    sign up

---
+++ Backend/signup.js
@@ -0,0 +1,22 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+const fdb = getFirestore(app);
+export const createUserHandler = async()=> {
+    try{
+        const response = await createUserWithEmailAndPassword(auth, email, password);
+                await setDoc(doc(fdb, 'users', fbauth.currentUser.uid), {
+                    firstName: '',
+                    lastName: '',
+                    email: email,
+                    password: password
+                });
+
+    }catch(error){
+        console.log(error); 
+        alert("Registration Failed:" + error.message)
+    }
+
+
+  
+}

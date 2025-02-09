080894c4faa0dde991209d343c24ef55601e296f
poojakedia <poojakedianov@gmail.com>
2025-02-08 20:14:32 -0500

    sign in and sign up handler

--- Frontend/u-o-me/src/handlers/signup.js
+++ Frontend/u-o-me/src/handlers/signup.js
@@ -4,13 +4,14 @@ import app from '../firebaseInit'
 
 const fdb = getFirestore(app);
 const auth = getAuth(app);
-const createUserHandler = async(name,email, password)=> {
+const createUserHandler = async(fname,lname,email, password)=> {
     try{
+        console.log(email);
         const response = await createUserWithEmailAndPassword(auth, email, password);
                 await setDoc(doc(fdb, 'users', auth.currentUser.uid), {
-                    name: name,
-                    email: email,
-                    password: password
+                    firstName: fname,
+                    lastName: lname,
+                    email: email
                 });
         console.log(response);
 

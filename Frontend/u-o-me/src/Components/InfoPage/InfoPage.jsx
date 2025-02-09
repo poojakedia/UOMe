b1a3e1742646879b6ea2ae821e27201abd3aefdb
Sanchita Agrawal <sanchitaagrawal362@gmail.com>
2025-02-09 01:22:28 -0500

    updates

---
+++ Frontend/u-o-me/src/Components/InfoPage/InfoPage.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import './InfoPage.css';
+
+import userInfoHandler from '../../handlers/userInfoHandler.js';
+import paymentHandler from '../../handlers/paymentHandler.js';
